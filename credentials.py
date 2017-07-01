60b6fbeb36de5012d4e62124bb7184c9b9c93e37
Sergey <iceman9831@gmail.com>
2017-07-01 17:36:59 +0300

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -3,4 +3,4 @@ import redis
 token = os.environ['TELEGRAM_TOKEN']
 admins = [os.environ['ADMIN1'], os.environ['ADMIN2'], os.environ['ADMIN3'], os.environ['ADMIN4'], os.environ['ADMIN5']]
 r = redis.from_url(os.environ.get("REDIS_URL"))
-p = redis.from_url(os.environ.get("HEROKU_REDIS_COBALT_URL"))
+p = redis.from_url(os.environ.get("COBALT_URL"))
